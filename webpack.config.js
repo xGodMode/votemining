a26aef4b485d6105a71daac070fe64c37c4044e8
valmack <8445610+valmack@users.noreply.github.com>
2021-02-27 23:15:53 -0500

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**
+ * @type { import('webpack').Configuration }
+ * */
+const config = {
+    target: 'node', // Node.js-context
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]',
+    },
+    optimization: {
+        minimize: false,
+    },
+    devtool: 'source-map',
+    externals: {
+        // Prevent bundling of certain imported packages and instead retrieve these external dependencies at runtime.
+        // excluding dependencies from the output bundles.
+        // Instead, the created bundle relies on that dependency to be present in the consumer's environment
+        web3: 'commonjs2 web3',
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: 'tsconfig.webpack.json',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.IgnorePlugin({ resourceRegExp: /^(?:electron)$/ }),
+        new webpack.IgnorePlugin({ resourceRegExp: /^(?:swarm-js)$/ }),
+    ],
+};
+
+module.exports = config;
